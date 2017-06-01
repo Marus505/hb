7300d6ece5e1da6352094211185d4e1e46c3ca1d
marus505 <roamlynx@gmail.com>
2017-06-01 15:11:04 +0900

    init

---
+++ src/main/java/com/mrs/HellobootApplication.java
@@ -0,0 +1,12 @@
+package com.mrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobootApplication.class, args);
+	}
+}
